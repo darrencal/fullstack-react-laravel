3977ad0b6c6bf78eddbc104f9f4616985bab4819
Darren Cal <darrencal64@gmail.com>
2022-06-06 17:14:31 -0600

    Added authentication with Laravel Passport

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('users', UserController::class);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::apiResource('users', UserController::class);
+});
