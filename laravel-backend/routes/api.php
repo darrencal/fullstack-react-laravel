c36d5cab04b0df23412a5a2dfc4e6eede70b0f57
Darren Cal <darrencal64@gmail.com>
2022-06-17 22:30:19 -0600

    Created product routes

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -27,4 +28,6 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::apiResource('users', UserController::class);
 
     Route::apiResource('roles', RoleController::class);
+
+    Route::apiResource('products', ProductController::class);
 });
